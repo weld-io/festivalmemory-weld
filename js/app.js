43845aab019b589048b4ae9555b1d55920fc5056
olgatar <leyendaorama@gmail.com>
2016-07-11 15:10:06 +0200

    add header and input form for personal number on the first page

--- js/app.js
+++ js/app.js
@@ -6,4 +6,25 @@ app.factory('Header', function() {
 
 function HeaderCtrl($scope, Header) {
   $scope.header = Header;
+  $scope.headerStyle = {
+    "font-family": "Alfa Slab One",
+    "color" : "#000",
+    "font-size" : "120px",
+    "text-align" : "center",
+    "text-shadow" : "none",
+    "margin-top" : "0px",
+    "padding" : "40px 0px"
+  }
 }
+
+app.controller('PersonalNrInputCtrl', [function() {
+
+  this.user = {
+       personalnumber: ''
+  };
+
+  this.register = function() {
+     console.log('User clicked next button', this.user);
+  };
+
+}]);
