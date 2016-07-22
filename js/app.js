3e0c3da7516f2e1bf0c261f6e2a606e62b049980
olgatar <leyendaorama@gmail.com>
2016-07-22 14:39:27 +0200

    update

--- js/app.js
+++ js/app.js
@@ -107,7 +107,7 @@ function imageURL(event){
 
 //Media queries for mobile
 if (matchMedia) {
-  var mq = window.matchMedia("screen and (max-width: 769px)");
+  var mq = window.matchMedia("screen and (max-device-width: 769px) and (orientation: portrait)");
   mq.addListener(WidthChange);
   WidthChange(mq);
 }
