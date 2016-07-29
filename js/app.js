82a6d99be6beedd34f03d40991d2737f7895bf79
olgatar <leyendaorama@gmail.com>
2016-07-29 10:57:29 +0200

    add throttle to domain search function add update css

--- js/app.js
+++ js/app.js
@@ -1,63 +1,72 @@
-var app = angular.module('app', ['ngAnimate', 'countrySelect','naif.base64']);
+var app = angular.module('app', ['ngAnimate', 'countrySelect','naif.base64','angular-lodash']);
 
-app.controller('MainCtrl', function($scope,$http,$window) {
+app.controller('MainCtrl', function($scope,$http,$window,$log) {
+
+  $scope.searchDomainShow = false;
+  $scope.seDomainShow = false;
+  $scope.nuDomainShow = false;
+  $scope.seButtonDisable = false;
+  $scope.nuButtonDisable = false;
 
   $scope.user = {};
 
-  $scope.DomainSearch = false;
-  $scope.AcceptTerms = false;
-  $scope.ImageDiv = false;
-  $scope.showDomainSE = false;
-  $scope.showDomainNU = false;
-  $scope.checkboxModel = {value : true};
-
-  $scope.showDomainSearch = function() {
-     $scope.DomainSearch = true;
-  };
+  $scope.acceptTermsShow = false;
+  $scope.imageDiv = false;
+  $scope.checkboxModel = {value : false};
 
   $scope.showImageDiv = function() {
-     $scope.ImageDiv = true;
+     $scope.imageDiv = true;
   };
 
-  $scope.searchDomain = function() {
+  $scope.searchDomain = _.throttle(function (domain){
+
+    $scope.searchDomainShow = true;
+
+    if (domain) {
 
       $http.get('https://domain-production-weld.herokuapp.com/api/domain/' + $scope.user.domain + '.se')
       .then(function(response) {
           //First function handles success (.se domain is available)
           $scope.freeDomainSE = "Välj!";
-          $scope.showDomainSE = true;
+          $scope.seDomainShow = true;
           var seButton = angular.element(document.querySelector('#seButton'));
           seButton.css('color','#47e2a1');
-          searchDomainNU();
+          $scope.searchDomainNU();
 
       }, function(response) {
           //Second function handles error (.se domain is not available)
           $scope.freeDomainSE = "Upptagen!";
-          $scope.showDomainSE = true;
+          $scope.seDomainShow = true;
           var seButton = angular.element(document.querySelector('#seButton'));
           seButton.css('color','#f86060');
-          searchDomainNU();
+          $scope.seButtonDisable = true;
+          $scope.searchDomainNU();
       });
-  };
+    };
+  },1000);
 
-  function searchDomainNU() {
+  $scope.searchDomainNU = _.throttle(function() {
+
+    if (domain) {
 
       $http.get('https://domain-production-weld.herokuapp.com/api/domain/' + $scope.user.domain + '.nu')
       .then(function(response) {
           //First function handles success (.nu domain is available)
           $scope.freeDomainNU = "Välj!";
-          $scope.showDomainNU = true;
+          $scope.nuDomainShow = true;
           var nuButton = angular.element(document.querySelector('#nuButton'));
           nuButton.css('color','#47e2a1');
 
       }, function(response) {
           //Second function handles error (.nu domain is not available)
           $scope.freeDomainNU = "Upptagen!";
-          $scope.showDomainNU = true;
+          $scope.nuDomainShow = true;
           var nuButton = angular.element(document.querySelector('#nuButton'));
           nuButton.css('color','#f86060');
+          $scope.nuButtonDisable = true;
       });
-  };
+    };
+  },1000);
 
   $scope.buttonClicked = function(domain,$event) {
       $scope.user.domain = domain;
@@ -68,8 +77,8 @@ app.controller('MainCtrl', function($scope,$http,$window) {
         $scope.termsUrl = "https://www.iis.se/docs/terms-and-conditions-nu.pdf";
         $scope.termsDomain = ".NU";
       }
-      $scope.DomainSearch = false;
-      $scope.AcceptTerms = true;
+      $scope.searchDomainShow = false;
+      $scope.acceptTermsShow = true;
       $scope.CreatePageButton = true;
   }
 
@@ -77,7 +86,6 @@ app.controller('MainCtrl', function($scope,$http,$window) {
     if ($scope.checkboxModel.value) {
       $scope.user.countrycode = $scope.user.countrycode.split(":").pop();
       $scope.user.tags = ["weekendfestival2016"];
-      $scope.user.pictureurl = "";
 
       var data = JSON.stringify($scope.user);
 
@@ -86,7 +94,7 @@ app.controller('MainCtrl', function($scope,$http,$window) {
           $scope.PostDataResponse = data;
 
           var pageUrl = "https://weld-staging.herokuapp.com/";
-          $window.location.href = pageUrl + data.projectSlug;
+          //$window.location.href = pageUrl + data.projectSlug;
       })
       .error(function (data, status, headers, config) {
           $scope.ResponseDetails = "Data: " + data +
@@ -104,20 +112,3 @@ function imageURL(event){
   var imagePath = URL.createObjectURL(event.target.files[0]);
   $('#image-div').css('background-image', 'url(' + imagePath + ')');
 }
-
-/* //Media queries for mobile
-if (matchMedia) {
-  var mq = window.matchMedia("screen and (max-width: 769px)");
-  mq.addListener(WidthChange);
-  WidthChange(mq);
-}
-
-function WidthChange(mq) {
-  if (mq.matches) {
-    // window width is less 769px
-    $('body').prepend('<div id="heading"><h1 id="heading-part1">FESTIVAL</h1><br><h1 id="heading-part2">MINNEN</h1></div>');
-  } else {
-    // window width is more than 769px
-    $('#heading').hide();
-  }
-} */
