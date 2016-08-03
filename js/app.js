53e7780e2eee288683a53dd61f3cce722a3550b4
olgatar <leyendaorama@gmail.com>
2016-08-03 14:19:57 +0200

    change weld-staging links for http post to the actual/live ones

--- js/app.js
+++ js/app.js
@@ -94,7 +94,7 @@ app.controller('MainCtrl', function($scope,$http,$window,$log) {
 
       var data = JSON.stringify($scope.user);
 
-      $http.post('https://weld-staging.herokuapp.com/api/users', data)
+      $http.post('https://www.weld.io/api/users', data)
       .success(function (data, status, headers, config) {
           $scope.PostDataResponse = data;
 
@@ -102,8 +102,8 @@ app.controller('MainCtrl', function($scope,$http,$window,$log) {
           $scope.showMainInput = false;
           $scope.showFinalPage = true;
 
-          var url = 'https://weld-staging.herokuapp.com/';
-          var snapshotUrl = 'https://weld-staging.herokuapp.com/exp-snapshot/';
+          var url = 'https://www.weld.io/';
+          var snapshotUrl = 'https://www.weld.io/exp-snapshot/';
 
           $scope.domain = $scope.user.domain.split(".")[0];
           $scope.userId = data.userId;
